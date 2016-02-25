941dd21079322cc1eb8f09d15633da9d0b5618f4
Simon Dell <simon@expressionworks.co.uk>
2016-02-25 17:33:05 +0000

    Updating three suggestion "slots" using three streams.

--- src/exercise.js
+++ src/exercise.js
@@ -1,3 +1,4 @@
+"use strict";
 const suggestions_list = document.querySelector( 'ul' );
 
 const refreshButton = document.querySelector('button');
@@ -18,15 +19,70 @@ var responseStream = requestStream
   });
 
 responseStream.subscribe(function(response) {
-  console.log( response.length );
-/*
-  const user_tmpl = document.querySelector('#user');
+  console.log( response );
 
-  user_tmpl.content.querySelector( 'li').textContent = response.length;
+  // const user_tmpl = document.querySelector('#user');
 
-  const new_li = document.importNode( user_tmpl.content, true );
-  suggestions_list.appendChild( new_li );
-*/
+  // user_tmpl.content.querySelector( 'li').textContent = response.length;
+
+  // const new_li = document.importNode( user_tmpl.content, true );
+  // suggestions_list.appendChild( new_li );
 });
 
+function pickRandomUser ( usersList ) {
+	return usersList[Math.floor(Math.random()*usersList.length)];
+}
 
+const suggestion1Stream = responseStream.
+	map( pickRandomUser ).
+	subscribe( user => {
+		const li = document.querySelectorAll( 'ul li' )[0];
+		li.textContent = user.login;
+	});
+
+const suggestion2Stream = responseStream.
+	map( pickRandomUser ).
+	subscribe( user => {
+		const li = document.querySelectorAll( 'ul li' )[1];
+		li.textContent = user.login;
+	});
+
+const suggestion3Stream = responseStream.
+	map( pickRandomUser ).
+	subscribe( user => {
+		const li = document.querySelectorAll( 'ul li' )[2];
+		li.textContent = user.login;
+	});
+
+
+
+
+// class SuggestionElement {
+// 	constructor ( elem ) {
+// 		this.elem = elem;
+// 	}
+
+// 	render ( data ) {
+// 		this.elem.textContent = data.login;
+// 	}
+// }
+
+// var suggestionIndeces = [0, 1, 2];
+
+// var suggestionViews = suggestionIndeces.map( index => {
+// 	new SuggestionElement( document.querySelectorAll( 'ul li' )[ index ] );
+// });
+
+// var suggestionSubscriptions = suggestionIndeces.map( index => {
+// 	responseStream.map(function(listUsers) {
+// 		// get one random user from the list
+// 		return listUsers[Math.floor(Math.random()*listUsers.length)];
+// 	});
+// });
+
+// suggestionSubscriptions.forEach( ( suggestionSubscription, index ) => {
+// console.log( index, suggestionSubscription );
+// 	suggestionSubscription.subscribe( user => {
+// 		suggestionViews[ index ].render( user );
+// 	});
+// });
